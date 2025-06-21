b9c1d9523d87a537354a1b45e0e1128750d9557f
Vasilev-VV <vasiavasilev97@mail.ru>
2025-06-21 14:02:20 +0700

    Выполнен алгоритм блок-схемы на c#

---
+++ Module12.Algorithm.Task12/Module12.Algorithm.Task12.ConsoleApp/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿namespace Module12;
+/// <summary>
+/// Пользователь
+/// </summary>
+public class User
+{
+    /// <summary>
+    /// Логин пользователя
+    /// </summary>
+    public string Login { get; set; }
+    /// <summary>
+    /// Имя пользователя
+    /// </summary>
+    public string Name { get; set; }
+    /// <summary>
+    /// Имеется ли премиум-подписка. True - есть премиум-подписка.
+    /// </summary>
+    public bool IsPremium { get; set; }
+}
--- Module12.Algorithm.Task12/Module12.Algorithm.Task12.ConsoleApp/Program.cs
+++ Module12.Algorithm.Task12/Module12.Algorithm.Task12.ConsoleApp/Program.cs
@@ -1,9 +1,69 @@
-﻿namespace Module12.Algorithm.Task12.ConsoleApp;
+﻿namespace Module12;
 
 public class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Введите количество пользователей:");
+        int countUser;
+        while (!(int.TryParse(Console.ReadLine(), out countUser) && countUser > 0))
+        {
+            Console.WriteLine("Количество пользователей введено не правильно.\nПопробуйте снова:");
+        }
+        User[] users = new User[countUser];
+        //ввод пользователей
+        for (int i = 0; i < users.Length; i++)
+        {
+            users[i] = new User();
+            Console.WriteLine($"Введите Login пользователя №{i + 1}:");
+            users[i].Login = Console.ReadLine();
+            Console.WriteLine($"Введите Name пользователя №{i + 1}:");
+            users[i].Name = Console.ReadLine();
+            Console.WriteLine($"Введите IsPremium (true/false) для пользователя №{i + 1}:");
+            string readIsPremium = Console.ReadLine();
+            while (readIsPremium != "true" && readIsPremium != "false")
+            {
+                Console.WriteLine("IsPremium пользователя введен не правильно.\nПопробуйте снова 'true' или 'false':");
+                readIsPremium = Console.ReadLine();
+            }
+            if (readIsPremium == "true")
+            {
+                users[i].IsPremium = true;
+            }
+            else if (readIsPremium == "false")
+            {
+                users[i].IsPremium = false;
+            }
+        }
+        //приветствие по имени
+        for (int i = 0; i < users.Length; i++)
+        {
+            Console.WriteLine();
+            if (users[i].IsPremium == false) ShowAds();            
+            Console.WriteLine($"Приветствую {users[i].Name}");
+            Console.WriteLine();
+        }
+
+        
+        Console.ReadKey();
+    }
+
+
+    /// <summary>
+    /// Метод для показа рекламы
+    /// </summary>
+    public static void ShowAds()
+    {
+        Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+        // Остановка на 1 с
+        Thread.Sleep(1000);
+
+        Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+        // Остановка на 2 с
+        Thread.Sleep(2000);
+
+        Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+        // Остановка на 3 с
+        Thread.Sleep(3000);
     }
 }
